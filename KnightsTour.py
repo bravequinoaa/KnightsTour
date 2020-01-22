a5e0a48c6843c39bc775e5c29c718b62afa6d9c0
wvano97 <49797942+wvano97@users.noreply.github.com>
2020-01-22 14:23:58 -0500

    Add files via upload

---
+++ KnightsTour.py
@@ -0,0 +1,76 @@
+# Board matrix size
+N = 8
+TARGETMOVES = N**2
+
+
+def checkValid(board, movx, movy):
+	'''
+		params:
+		movx => next x move
+		movy => next y move
+		
+		checks if move is valid
+	'''
+	if (movx >= 0 and movy >= 0 and movx < N and movy < N and board[movx][movy] == -1):
+		return True
+	return False
+
+
+def printBoard(board):
+	'''
+		Prints Board
+	'''
+	for i in range(len(board)):
+		print(board[i])
+
+
+def KnightsTour():
+	currx = 0
+	curry = 0
+
+	# Init board
+	board = [[-1 for i in range(N)] for i in range(N)]
+
+	xmoves = [-2, -2, -1, -1, 1, 1, 2, 2]
+	ymoves = [1, -1, 2, -2, 2, -2, 1, -1]
+
+	totalmoves = 1
+
+	board[0][0] = 0
+
+	if generateMove(board, currx, curry, totalmoves, xmoves, ymoves):
+		printBoard(board)
+	else: print("Invalid")
+	
+
+	
+
+
+def generateMove(board, currx, curry, totalmoves, xmoves, ymoves):
+	if totalmoves == TARGETMOVES:
+		return True
+
+	print("X: {} <> Y: {}".format(currx, curry))
+
+
+	for i in range(8):
+
+		nextx = currx + xmoves[i]
+		nexty = curry + ymoves[i]
+
+		if checkValid(board, nextx, nexty):
+			board[nextx][nexty] = totalmoves
+
+			if generateMove(board, nextx, nexty, totalmoves+1, xmoves, ymoves):
+				return True
+			board[nextx][nexty] = -1
+
+	return False
+
+
+
+
+#printBoard()
+
+if __name__ == "__main__":
+	KnightsTour()
---
+++ KnightsTourGUI.py
@@ -0,0 +1,133 @@
+import pygame
+
+# Prob have to slow down (sleep?)
+# timer
+# arrow for path
+
+# Init Gui
+
+pygame.init()
+
+
+# Window Size
+WIN_DIMENSION = 800
+
+win = pygame.display.set_mode((WIN_DIMENSION, WIN_DIMENSION))
+pygame.display.set_caption("Knights Tour")
+
+
+# Board Size
+N = 8
+
+TARGETMOVES = N**2
+
+# Colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (180, 0, 0)
+
+def DisplayGui(board):
+	# run = True
+	# while run:
+	win.fill(black)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			# run = False
+			print("CLOSING")
+
+	for i in range(N):
+		ydraw = i * (WIN_DIMENSION/N)
+		for n in range(N):
+			xdraw = n * (WIN_DIMENSION / N)
+			pygame.draw.rect(win, red, (xdraw, ydraw, WIN_DIMENSION/N, WIN_DIMENSION/N), 3)
+			displayText(board[i][n], xdraw, ydraw)
+			
+
+
+	
+	pygame.display.update()
+
+
+def text_objects(text, font):
+	textSurface = font.render(str(text), True, white)
+	return textSurface, textSurface.get_rect()
+
+
+def displayText(text, xdraw, ydraw):
+	font = pygame.font.Font('freesansbold.ttf', 7 * N)
+	TextSurf, TextRect = text_objects(text, font)
+	TextRect.center = ((xdraw + (WIN_DIMENSION / N) / 2), (ydraw + (WIN_DIMENSION / N) / 2))
+	win.blit(TextSurf, TextRect)
+
+
+def checkValid(board, movx, movy):
+	'''
+		params:
+		movx => next x move
+		movy => next y move
+		
+		checks if move is valid
+	'''
+	if (movx >= 0 and movy >= 0 and movx < N and movy < N and board[movx][movy] == " "):
+		return True
+	return False
+
+
+def printBoard(board):
+	'''
+		Prints Board
+	'''
+	for i in range(len(board)):
+		print(board[i])
+
+
+def KnightsTour():
+	currx = 0
+	curry = 0
+
+	# Init board
+	board = [[" " for i in range(N)] for i in range(N)]
+
+	xmoves = [-2, -2, -1, -1, 1, 1, 2, 2]
+	ymoves = [1, -1, 2, -2, 2, -2, 1, -1]
+
+	totalmoves = 1
+
+	board[0][0] = 0
+	DisplayGui(board)
+
+	if generateMove(board, currx, curry, totalmoves, xmoves, ymoves):
+		printBoard(board)
+		DisplayGui(board)
+	else: print("Invalid")
+	
+
+	
+
+
+def generateMove(board, currx, curry, totalmoves, xmoves, ymoves):
+	if totalmoves == TARGETMOVES:
+		return True
+
+	print("X: {} <> Y: {}".format(currx, curry)) # draw  board here
+
+
+	for i in range(8):
+
+		nextx = currx + xmoves[i]
+		nexty = curry + ymoves[i]
+
+		if checkValid(board, nextx, nexty):
+			board[nextx][nexty] = totalmoves
+
+			if generateMove(board, nextx, nexty, totalmoves+1, xmoves, ymoves):
+				return True
+			# backtracking
+			board[nextx][nexty] = " "
+
+	return False
+
+
+if __name__ == "__main__":
+	KnightsTour()
+	
